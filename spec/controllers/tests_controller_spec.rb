05f8196851e42bc24171c533001f1020a2fa5152
Caleb Cohoon <calebcohoon@gmail.com>
2012-04-26 22:58:51 -0700

    Partial's name and path are now appended to the end of a partial's content.

--- spec/controllers/tests_controller_spec.rb
+++ spec/controllers/tests_controller_spec.rb
@@ -3,9 +3,13 @@ require 'spec_helper'
 describe TestsController do
   render_views
 
+  before { get :index }
+
   it 'adds ths file path to the partial' do
-    pending
-    get :index
-    response.body.should =~ /_partial.html.erb/
+    response.body.should =~ /app\/views\/tests\/_partial.html.erb/
+  end
+
+  it 'does not show the path for the view' do
+    response.body.should_not =~ /index.html.erb/
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ ENV["RAILS_ENV"] ||= 'test'
 
 require File.expand_path("../test_app/config/environment", __FILE__)
 require 'rspec/rails'
+require 'awesome_print'
+require 'way'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
